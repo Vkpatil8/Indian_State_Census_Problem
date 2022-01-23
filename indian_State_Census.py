2f0367cc2c161ce6a75361699b19201200ad9903
vkpatil <vishalvkpatil8@gmail.com>
2022-01-23 21:44:42 +0530

    [vishal] Added: test case for checking file extension

--- indian_State_Census.py
+++ indian_State_Census.py
@@ -2,8 +2,8 @@
 @Author: Vishal Patil
 @Date: 23-01-2022 13:45:00
 @Last Modified by: Vishal Patil
-@Last Modified time: 23-01-2022 18:45:00
-@Title : Solving use case 1 test case 1
+@Last Modified time: 23-01-2022 21:45:00
+@Title : Solving use case 1 test case 2
 """
 
 import csv
