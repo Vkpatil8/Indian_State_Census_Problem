65034fc3da3b983f821b5a878337e12f2b1148c4
vkpatil <vishalvkpatil8@gmail.com>
2022-01-23 17:30:52 +0530

    [vishal] Added: load and count

--- Indian_State_Census_Problem.py
+++ Indian_State_Census_Problem.py
@@ -1,5 +1,48 @@
 """
 @Author: Vishal Patil
 @Date: 23-01-2022 13:45:00
+@Last Modified by: Vishal Patil
+@Last Modified time: 23-01-2022 16:00:00
+@Title : Solving use case 1
 """
 
+import csv
+
+
+class StateCensusAnalyser:
+    @staticmethod
+    def state_census_loader():
+        with open("StateCensusData.csv") as data:
+            state_census = csv.reader(data)
+            for info in state_census:
+                print(info)
+
+    @staticmethod
+    def count_number_of_records():
+        with open("StateCensusData.csv") as data:
+            state_census = csv.reader(data)
+            return len(list(state_census))
+
+
+class CSVStates:
+    @staticmethod
+    def state_code_loader():
+        with open("StateCode.csv", "r") as data:
+            state_code = csv.reader(data)
+            for info in state_code:
+                print(info)
+
+    @staticmethod
+    def count_number_of_records():
+        with open("StateCode.csv") as data:
+            state_code = csv.reader(data)
+            return len(list(state_code))
+
+
+if __name__ == '__main__':
+    file_name = "StateCensusData.csv"
+    file_name1 = "StateCode.csv"
+    StateCensusAnalyser.state_census_loader()
+    print(StateCensusAnalyser.count_number_of_records())
+    CSVStates.state_code_loader()
+    print(CSVStates.count_number_of_records())
