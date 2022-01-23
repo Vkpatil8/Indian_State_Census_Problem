74a05ff0db8b9045667e7787a6d8c522442c2300
vkpatil <vishalvkpatil8@gmail.com>
2022-01-23 22:06:25 +0530

    [vishal] Added: test case for checking incorrect file extension

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import indian_State_Census
+import unittest
 
 
-def test_number_of_record():
-    result = indian_State_Census.StateCensusAnalyser.count_number_of_records()
-    expected = 30
-    assert expected == result
+class TestCases(unittest.TestCase):
+    def test_number_of_record(self):
+        result = indian_State_Census.StateCensusAnalyser.count_number_of_records()
+        expected = 30
+        self.assertEqual(expected, result)
 
+    def test_check_file_extension(self):
+        result = indian_State_Census.StateCensusAnalyser.file_extension()
+        expected = ".csv"
+        self.assertEqual(result, expected)
 
-def test_check_file_extension():
-    result = indian_State_Census.StateCensusAnalyser.file_extension()
-    expected = ".csv"
-    assert expected == result
+    def test_check_file_extension_false(self):
+        result = indian_State_Census.StateCensusAnalyser.file_extension()
+        expected = ".txt"
+        self.assertNotEqual(result, expected)
+
