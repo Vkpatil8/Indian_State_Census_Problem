0d97f4f25dc6fd266b30659e2cbd0303b167cd66
vkpatil <vishalvkpatil8@gmail.com>
2022-01-24 17:56:44 +0530

    [vishal] Added: load the Indian

--- indian_State_Code.py
+++ indian_State_Code.py
@@ -1,27 +1,17 @@
-"""
-@Author: Vishal Patil
-@Date: 23-01-2022 13:45:00
-@Last Modified by: Vishal Patil
-@Last Modified time: 24-01-2022 18:00:00
-@Title : Solving use case 2 & all use cases
-"""
-
 import csv
 
-
-class IndianCensusException(Exception):
-    pass
+from Indian_State_Census_Problem.indian_State_Census import IndianCensusException
 
 
-class StateCensusAnalyser:
+class CSVStates:
     @staticmethod
-    def state_census_loader():
+    def state_code_loader():
         """
             desc: method to load records in file
         """
-        with open("StateCensusData.csv") as data:
-            state_census = csv.reader(data)
-            for info in state_census:
+        with open("StateCode.csv", "r") as data:
+            state_code = csv.reader(data)
+            for info in state_code:
                 print(info)
 
     @staticmethod
@@ -29,9 +19,9 @@ class StateCensusAnalyser:
         """
             desc: method to count number of records in file
         """
-        with open("StateCensusData.csv") as data:
-            state_census = csv.reader(data)
-            return len(list(state_census))
+        with open("StateCode.csv") as data:
+            state_code = csv.reader(data)
+            return len(list(state_code))
 
     @staticmethod
     def file_extension(file):
@@ -69,7 +59,5 @@ class StateCensusAnalyser:
 
 
 if __name__ == '__main__':
-    file_name = "StateCensusData.csv"
-    file_name1 = "StateCode.csv"
-    StateCensusAnalyser.state_census_loader()
-    print(StateCensusAnalyser.count_number_of_records())
+    CSVStates.state_code_loader()
+    print(CSVStates.count_number_of_records())
---
+++ test_indian_state_census.py
@@ -0,0 +1,62 @@
+import indian_State_Census
+import unittest
+
+
+class TestCases1(unittest.TestCase):
+
+    def test_number_of_record(self):
+        """
+            desc: test the method to count number of records in file
+        """
+        result = indian_State_Census.StateCensusAnalyser.count_number_of_records()
+        expected = 30
+        self.assertEqual(expected, result)
+
+    def test_check_file_extension(self):
+        """
+            desc: test the method to check file extension
+        """
+        result = indian_State_Census.StateCensusAnalyser.file_extension("StateCensusData.csv")
+        expected = ".csv"
+        self.assertEqual(result, expected)
+
+    def test_check_file_extension_false(self):
+        """
+         desc: test the method to raise exception while checking file extension
+        """
+        method = indian_State_Census.StateCensusAnalyser.file_extension
+        self.assertRaises(indian_State_Census.IndianCensusException, method, "abc.txt")
+
+    def test_match_delimiter(self):
+        """
+            desc: test the method to check delimiter
+        """
+        expected = ','
+        result = indian_State_Census.StateCensusAnalyser.delimiter_validation("StateCensusData.csv")
+        self.assertEqual(result, expected)
+
+    def test_not_match_delimiter(self):
+        """
+            desc: test the method to raise exception while checking delimiter
+        """
+        method = indian_State_Census.StateCensusAnalyser.delimiter_validation
+        self.assertRaises(indian_State_Census.IndianCensusException, method, "StateCensusData.txt")
+
+    def test_match_header(self):
+        """
+            desc: test the method to check header
+        """
+        expected = True
+        result = indian_State_Census.StateCensusAnalyser.validate_header("StateCensusData.csv")
+        self.assertEqual(result, expected)
+
+    def test_not_match_header(self):
+        """
+            desc: test the method to raise exception while checking headers
+        """
+        method = indian_State_Census.StateCensusAnalyser.validate_header
+        self.assertRaises(indian_State_Census.IndianCensusException, method, "StateCensusData1.csv")
+
+
+if __name__ == '__main__':
+    unittest.main()
